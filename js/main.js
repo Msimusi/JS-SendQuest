4cdbc1fafaa965e30f00f087820ca88a42134e2e
Msimusi <120442998+Msimusi@users.noreply.github.com>
2023-02-27 01:31:27 +0900

    1차 배포

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 const letters = [
-  "Letter 1",
+  `설마 첫날부터 실패하진 않았겠지? 하는 마음으로 편지를 남긴다. 
+  이게 날짜가 아니라 순번을 기준으로 삼아놔가지고, 정확하게 언제 볼거다 라고 예상하기는 어렵네. 
+  아무튼 어떻게 편지를 적어서 전달할까 고민하다가 코딩도 배우는 중인데 하면서 최소한의 기능만 구현하며 뚝딱 해봤다. 
+  
+  너가 그동안 생활패턴 문제로 얼마나 고생했는지 알고 있고 그래서 제대로 성공했으면 한다.
+  그렇다고 또 너무 부담갖고 맘고생하지는 말고, 나름 재밌더라. 이런거 만드는거
+  달성 기념 선물은 자동으로 주게 짜는 실력은 못되고, 14번의 버튼 중 어딘가에 내 약속을 이런 편지로 박아둘거다. 그거 보여주면 돼
+  솔직히 말하자면 아직 나도 몇번 했을 때 무엇을 줄 것인지 결정한 것은 없어~
+  
+  우선 대충 확인할 수 있는 것들은 얼추 확인해봤는데, 혹시 정상작동하지 않는 부분이 있으면 따로 말해줘.
+  
+  여튼 뭐, 힘내자 파이팅 27th Feb
+
+  앞으로 할 작업들
+  - 편지들 (쒯)
+  - 편지 작성했을 경우 해당 버튼에 편지 아이콘 출력하기
+  - 편지를 좀 더 가독성 있게 전달하는 법 궁리..
+  `,
   "Letter 2",
   "Letter 3",
   "Letter 4",
@@ -37,33 +54,29 @@ buttons.forEach((button, number) => {
     button.innerText = "Done!";
   }
 
+  const todayNoMore = Boolean(achievements.date != now.getDate());
+
   const timeOk = Boolean(
-    hour >= 5
-    // (hour === 5 && min >= 30) || (hour === 6 && min <= 30)
+    (hour === 5 && min >= 30) || (hour === 6 && min <= 30)
   );
   const rightOrder = Boolean(number === achievements.count);
 
-  if (timeOk && rightOrder) {
+  if (timeOk && rightOrder && todayNoMore) {
     button.classList.remove("disabled");
     button.innerText = "기상!";
   }
 });
-
-// window.addEventListener("load", buttonClassify);
+// };
 
 const taskToDo = document.querySelector("button.not-confirmed:not(.disabled)");
 
 const taskConfirm = () => {
-  if (now <= achievements.date + TimeToReupload) {
-    alert("Can confirm only once a day");
-  } else {
-    achievements.count++;
-    achievements.date = now;
-    localStorage.setItem("achievements", JSON.stringify(achievements));
-    // buttonClassify();
-    alert(letters[achievements.count - 1]);
-    location.reload();
-  }
+  achievements.count++;
+  achievements.date = now.getDate();
+  localStorage.setItem("achievements", JSON.stringify(achievements));
+  // buttonClassify();
+  alert(letters[achievements.count - 1]);
+  location.reload();
 };
 
 if (taskToDo) {
@@ -83,7 +96,11 @@ const taskDisabled = document.querySelectorAll("button.disabled");
 
 taskDisabled.forEach((button, index) => {
   const notAvailable = () => {
-    alert("Not Available");
+    if (achievements.date === now.getDate()) {
+      alert("하루에 한 번만 인증 가능합니다!");
+    } else {
+      alert("정해진 시간이 아니거나 아직 차례가 아닙니다.");
+    }
   };
   button.addEventListener("click", notAvailable);
 });
@@ -95,4 +112,5 @@ const reinitialize = () => {
   location.reload();
 };
 
+// window.addEventListener("load", buttonClassify);
 reinitializeBtn.addEventListener("click", reinitialize);
